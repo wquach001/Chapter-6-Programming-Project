2c70fc52292fa044787a7995c2597968fafd7c7c
Wai Quach <wyquach5@gmail.com>
2025-08-04 22:17:29 -0400

    i

--- Chapter 6 Programming Project.cpp
+++ Chapter 6 Programming Project.cpp
@@ -4,54 +4,90 @@
 #include <iostream>
 using namespace std;
 
-int getScore();
-bool isLower(int value1, int value2);
-bool isHigher(int value1, int value2);
-int calcAverage(int total, int minScore, int maxScore);
+const double galpft = 1.0 / 110.0;
+const double labpft = 8.0 / 110.0;
+const int laborChargePerHour = 25; // Cost of labor per hour
+
+double getSqFt();
+int gallonsForRoom(int squareFeet);
+double getPricePerGallon();
+int getRooms();
+void displayEstimate(int paintCharge, int gallonsNeeded, int laborCharge, int laborHours, int numrooms);
 
 int main()
 {
-	const int totScores = 5; // Total number of scores
-	int minScore = 0;
-	int maxScore = 0;
-	int score = 0;
-	int average = 0;
-	int total = 0;
-	for (int i = 0; i < totScores; i++) // Loop to get 5 scores
-	{
-		score = getScore();
-		total += score;
+	double paintCharge = 0;
+	double laborCharge = 0;
+	int gallonsNeeded = 0;
+	double laborHours = 0;
 
-		if (isLower(score, minScore))
-			minScore = score;
-		if (isHigher(score, maxScore))
-			maxScore = score;
-	}
-	average = calcAverage(total, minScore, maxScore);
-	cout << "The average score is: " << average << endl;
+	int numrooms = getRooms();
+	int squareFeet = getSqFt();
+	int gallons = gallonsForRoom(squareFeet);
+	gallonsNeeded += gallons;
+
+	double paintPrice = getPricePerGallon();
+	paintCharge += gallons * paintPrice;
+
+	double labor = squareFeet * labpft;
+	laborHours += labor;
+
+	laborCharge = laborCharge * laborHours;
+	void displayEstimate(int paintCharge, int gallonsNeeded, int laborCharge, int laborHours, int numrooms);
+
 	return 0;
 }
-int getScore()
+double getSqFt()
 {
-	int score;
-	cout << "Enter your score: ";
-	cin >> score;
-	if (score < 0 || score > 10) // Assuming scores are between 0 and 10
-	{
-		cout << "Invalid score. Please enter a score between 0 and 10." << endl;
-		cin >> score; // Re-prompt for valid score
+	double squareFeet = 0;
+	cout << "Enter the square footage of the room: ";
+	cin >> squareFeet;
+	if (squareFeet < 0) {
+		cout << "Invalid square footage. Please enter a positive number." << endl;
+		cin >> squareFeet;
 	}
-	return score; // Return the valid score
+	return squareFeet;
 }
-bool isLower(int value1, int value2)
+int gallonsForRoom(int squareFeet)
 {
-	return value1 < value2;
+	double gallons = 0;
+	gallons = squareFeet * galpft;
+	if (gallons < 1) {
+		gallons = 1; // At least one gallon is needed
+	}
+	return gallons;
 }
-bool isHigher(int value1, int value2)
+double getPricePerGallon()
 {
-	return value1 > value2;
+	double paintPrice = 0;
+	cout << "Enter the price per gallon of paint: ";
+	cin >> paintPrice;
+	if (paintPrice <= 10) 
+	{
+		cout << "Invalid price. Please enter a price greater than $10." << endl;
+		cin >> paintPrice;
+	}
+	return paintPrice;
 }
-int calcAverage(int total, int minScore, int maxScore)
+int getRooms()
 {
-	return (total - minScore - maxScore) / 3; // Assuming 5 scores, excluding min and max
+	int numrooms = 0;
+	cout << "Enter the number of rooms to be painted: ";
+	cin >> numrooms;
+	if (numrooms < 1) {
+		cout << "Invalid number of rooms. Please enter a positive number." << endl;
+		cin >> numrooms;
+	}
+	return numrooms;
 }
+void displayEstimate(double paintCharge, int gallonsNeeded, double laborCharge, double laborHours, int numrooms)
+{
+	cout << "Estimate for painting " << numrooms << " room(s):" << endl;
+	cout << "Gallons of paint to purchase: " << gallonsNeeded << endl;
+	cout << "Number of hours required to paint rooms: " << laborHours << endl;
+	cout << "Cost of paint: $" << paintCharge << endl;
+	cout << "Cost of labor: $" << laborCharge << endl;
+	cout << "Total cost for job: $" << (paintCharge + laborCharge) << endl;
+
+}
+
