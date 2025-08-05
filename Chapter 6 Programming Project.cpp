f93ad9fa142ae747fc285f26bece32dd412c90e8
Wai Quach <wyquach5@gmail.com>
2025-08-04 21:10:44 -0400

    change

--- Chapter 6 Programming Project.cpp
+++ Chapter 6 Programming Project.cpp
@@ -3,25 +3,55 @@
 
 #include <iostream>
 using namespace std;
-#include <iomanip>
 
-double getCelsius(double farenheit);
-double farenheit, celsius;
+int getScore();
+bool isLower(int value1, int value2);
+bool isHigher(int value1, int value2);
+int calcAverage(int total, int minScore, int maxScore);
+
 int main()
 {
-	cout << setprecision(1) << fixed << showpoint;
-	cout << "\tFarenheit" << setw(10) << "\tCelsius" << endl;
-	cout << "-----------------------------------" << endl;
-
-	for (double farenheit = 0; farenheit <= 20; farenheit++) 
+	const int totScores = 5; // Total number of scores
+	int minScore = 0;
+	int maxScore = 0;
+	int score = 0;
+	int average = 0;
+	int total = 0;
+	for (int i = 0; i < totScores; i++) // Loop to get 5 scores
 	{
-		getCelsius(farenheit);
-		cout << right << setw(15) << farenheit << setw(15) << celsius << endl;
+		score = getScore();
+		total += score;
+
+		if (isLower(score, minScore))
+			minScore = score;
+		if (isHigher(score, maxScore))
+			maxScore = score;
 	}
+	average = calcAverage(total, minScore, maxScore);
+	cout << "The average score is: " << average << endl;
 	return 0;
 }
-double getCelsius(double farenheit)
+int getScore()
 {
-	celsius = (farenheit - 32) * 5 / 9;
-	return celsius;
+	int score;
+	cout << "Enter your score: ";
+	cin >> score;
+	if (score < 0 || score > 10) // Assuming scores are between 0 and 10
+	{
+		cout << "Invalid score. Please enter a score between 0 and 10." << endl;
+		cin >> score; // Re-prompt for valid score
+	}
+	return score; // Return the valid score
+}
+bool isLower(int value1, int value2)
+{
+	return value1 < value2;
+}
+bool isHigher(int value1, int value2)
+{
+	return value1 > value2;
+}
+int calcAverage(int total, int minScore, int maxScore)
+{
+	return (total - minScore - maxScore) / 3; // Assuming 5 scores, excluding min and max
 }
