cb4f188a24aafb9875b2a05b8bbc263dfae239b3
Wai Quach <wyquach5@gmail.com>
2025-08-04 23:13:04 -0400

    u

--- Chapter 6 Programming Project.cpp
+++ Chapter 6 Programming Project.cpp
@@ -6,34 +6,39 @@ using namespace std;
 
 const double galpft = 1.0 / 110.0;
 const double labpft = 8.0 / 110.0;
-const int laborChargePerHour = 25; // Cost of labor per hour
+const double laborChargePerHour = 25.0; // Cost of labor per hour
 
 double getSqFt();
-int gallonsForRoom(int squareFeet);
+int gallonsForRoom(double squareFeet);
 double getPricePerGallon();
 int getRooms();
-void displayEstimate(int paintCharge, int gallonsNeeded, int laborCharge, int laborHours, int numrooms);
+void displayEstimate(double paintCharge, int gallonsNeeded, double laborCharge, double laborHours, int numrooms);
 
 int main()
 {
 	double paintCharge = 0;
 	double laborCharge = 0;
+	int gallons = 0;
 	int gallonsNeeded = 0;
 	double laborHours = 0;
+	double squareFeet = 0.0;
+	double totalSquareFeet = 0.0;
 
 	int numrooms = getRooms();
-	int squareFeet = getSqFt();
-	int gallons = gallonsForRoom(squareFeet);
-	gallonsNeeded += gallons;
+	for (int i=0; i < numrooms; i++) {
+		cout << "Sq Feet Room " << (i + 1) << ":" << endl;
+		squareFeet = getSqFt();
+		totalSquareFeet += squareFeet;
+	}
 
+	gallonsNeeded = gallonsForRoom(totalSquareFeet);
 	double paintPrice = getPricePerGallon();
-	paintCharge += gallons * paintPrice;
+	paintCharge += gallonsNeeded * paintPrice;
 
-	double labor = squareFeet * labpft;
-	laborHours += labor;
+	 laborHours = totalSquareFeet * labpft;
 
-	laborCharge = laborCharge * laborHours;
-	void displayEstimate(int paintCharge, int gallonsNeeded, int laborCharge, int laborHours, int numrooms);
+	laborCharge = laborChargePerHour * laborHours;
+	displayEstimate (paintCharge, gallonsNeeded, laborCharge, laborHours, numrooms);
 
 	return 0;
 }
@@ -48,14 +53,16 @@ double getSqFt()
 	}
 	return squareFeet;
 }
-int gallonsForRoom(int squareFeet)
+#include <cmath> // For std::ceil
+// Function to calculate the number of gallons needed for a room based on its square footage
+int gallonsForRoom (double squareFeet)
 {
-	double gallons = 0;
-	gallons = squareFeet * galpft;
-	if (gallons < 1) {
-		gallons = 1; // At least one gallon is needed
+	int galNeeded = 0;
+	galNeeded = (int) std::ceil(squareFeet * galpft);
+	if (galNeeded < 1) {
+		galNeeded = 1; // At least one gallon is needed
 	}
-	return gallons;
+	return galNeeded;
 }
 double getPricePerGallon()
 {
@@ -69,18 +76,19 @@ double getPricePerGallon()
 	}
 	return paintPrice;
 }
+
 int getRooms()
 {
-	int numrooms = 0;
+	int numRooms = (int) 0;
 	cout << "Enter the number of rooms to be painted: ";
-	cin >> numrooms;
-	if (numrooms < 1) {
+	cin >> numRooms;
+	if (numRooms < 1) {
 		cout << "Invalid number of rooms. Please enter a positive number." << endl;
-		cin >> numrooms;
+		cin >> numRooms;
 	}
-	return numrooms;
+	return numRooms;
 }
-void displayEstimate(double paintCharge, int gallonsNeeded, double laborCharge, double laborHours, int numrooms)
+void displayEstimate (double paintCharge, int gallonsNeeded, double laborCharge, double laborHours, int numrooms)
 {
 	cout << "Estimate for painting " << numrooms << " room(s):" << endl;
 	cout << "Gallons of paint to purchase: " << gallonsNeeded << endl;
